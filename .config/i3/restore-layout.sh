a67ebbf11c9f526481bb3eac6ef311cb92069f5e
Jackson Delahunt <jackson@stemn.com>
2019-10-27 16:35:05 +1100

    sleep

---
+++ .config/i3/restore-layout.sh
@@ -0,0 +1,9 @@
+i3-resurrect restore -w 1
+i3-resurrect restore -w 2
+i3-resurrect restore -w 3
+i3-resurrect restore -w 4
+i3-resurrect restore -w 5
+i3-resurrect restore -w 6
+i3-resurrect restore -w 7
+i3-resurrect restore -w 8
+i3-msg workspace 1
