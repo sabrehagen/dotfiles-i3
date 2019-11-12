76b71d81dbf679bd4905e166e677c35a95a177e2
Jackson Delahunt <jackson@stemn.com>
2019-11-12 11:21:22 +1100

    software

---
+++ .config/i3/status.sh
@@ -0,0 +1,6 @@
+i3status | while :
+do
+  read line
+  DESKTOP_ENVIRONMENT_BUILD=$(timeago $(cat ~/repositories/sabrehagen/desktop-environment/.dotfiles-cachebust))
+  echo "Built $DESKTOP_ENVIRONMENT_BUILD | $line" || exit 1
+done
