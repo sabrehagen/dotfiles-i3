0c74795fb2e4ae38b75afe81cf38bbd1f018e12e
Jackson Delahunt <jackson@stemn.com>
2023-11-17 21:51:16 +1100

    handle-windows-before-focused

--- .config/i3/move-to-next-window-of-type.sh
+++ .config/i3/move-to-next-window-of-type.sh
@@ -14,14 +14,23 @@ for WORKSPACE_NUMBER in $WORKSPACE_NUMBERS; do
 
   # If searching the current workspace, only search for windows after the focused window
   if [ "$WORKSPACE_NUMBER" -eq "$CURRENT_WORKSPACE" ]; then
-    WORKSPACE_WINDOWS_AFTER_FOCUSED_WINDOW=$(xdotool search --all --onlyvisible --desktop $(xprop -notype -root _NET_CURRENT_DESKTOP | cut -c 24-) "" 2>/dev/null | xargs -I@ zsh -c "echo @-\$(xprop -id @ | grep WM_CLASS | cut -d'\"' -f2)" | awk "/$FOCUSED_WINDOW.*/{p=1;next} p")
 
+    WORKSPACE_WINDOWS_AFTER_FOCUSED_WINDOW=$(xdotool search --all --onlyvisible --desktop $(( $WORKSPACE_NUMBER - 1 )) "" 2>/dev/null | xargs -I@ sh -c "echo @-\$(xprop -id @ | grep WM_CLASS | cut -d'\"' -f2)" | awk "/$FOCUSED_WINDOW.*/{p=1;next} p")
     WORKSPACE_MATCHING_WINDOW=$(echo $WORKSPACE_WINDOWS_AFTER_FOCUSED_WINDOW | tr ' ' \\n | grep $WINDOW_CLASS | cut -d- -f1 | head -n1)
+
   fi
 
   if [ -n "$WORKSPACE_MATCHING_WINDOW" ]; then
     xdotool windowactivate $WORKSPACE_MATCHING_WINDOW
-    break
+    exit
   fi
 
 done
+
+# If a matchine window was not found, check windows before the focused window on the current workspace
+WORKSPACE_WINDOWS_BEFORE_FOCUSED_WINDOW=$(xdotool search --all --onlyvisible --desktop $(( $WORKSPACE_NUMBER - 1 )) "" 2>/dev/null | xargs -I@ sh -c "echo @-\$(xprop -id @ | grep WM_CLASS | cut -d'\"' -f2)" | awk "/$FOCUSED_WINDOW.*/{found=1} !found{print}")
+WORKSPACE_MATCHING_WINDOW=$(echo $WORKSPACE_WINDOWS_BEFORE_FOCUSED_WINDOW | tr ' ' \\n | grep $WINDOW_CLASS | cut -d- -f1 | head -n1)
+
+if [ -n "$WORKSPACE_MATCHING_WINDOW" ]; then
+  xdotool windowactivate $WORKSPACE_MATCHING_WINDOW
+fi
