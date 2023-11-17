ee2dde831dc2693b76bbc2b71d34acce58205003
Jackson Delahunt <jackson@stemn.com>
2023-11-17 18:02:22 +1100

    revert-next-window-changes

--- .config/i3/move-to-next-window-of-type.sh
+++ .config/i3/move-to-next-window-of-type.sh
@@ -4,26 +4,16 @@ CURRENT_WORKSPACE=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).
 PREVIOUS_WORKSPACES=$(i3-msg -t get_workspaces | jq '.[].num' | sort -n | awk -v current=$CURRENT_WORKSPACE '$1 < current')
 NEXT_WORKSPACES=$(i3-msg -t get_workspaces | jq '.[].num' | sort -n | awk -v current=$CURRENT_WORKSPACE '$1 > current')
 
-WORKSPACE_NUMBERS="$CURRENT_WORKSPACE $NEXT_WORKSPACES $PREVIOUS_WORKSPACES"
+WORKSPACE_NUMBERS="$NEXT_WORKSPACES $PREVIOUS_WORKSPACES $CURRENT_WORKSPACE"
 
 FOCUSED_WINDOW=$(xdotool getactivewindow 2>/dev/null || echo no-focused-window)
-FOCUSED_WINDOW_CLASS=$(xdotool getactivewindow | xargs -n1 xprop WM_CLASS -id | cut -d'"' -f2 2>/dev/null || echo no-focused-window)
 
 for WORKSPACE_NUMBER in $WORKSPACE_NUMBERS; do
 
-  WORKSPACE_MATCHING_WINDOWS=$(xdotool search --desktop $(( $WORKSPACE_NUMBER - 1 )) --class "$WINDOW_CLASS|$FOCUSED_WINDOW_CLASS")
+  MATCHING_WORKSPACE_WINDOW=$(xdotool search --desktop $(( $WORKSPACE_NUMBER - 1 )) --class $WINDOW_CLASS | grep -v $FOCUSED_WINDOW | head -1)
 
-  # If searching the current workspace, only search for windows after the focused window
-  if [ "$WORKSPACE_NUMBER" -eq "$CURRENT_WORKSPACE" ]; then
-
-    WORKSPACE_MATCHING_WINDOWS=$(echo $WORKSPACE_MATCHING_WINDOWS | tr ' ' \\n | awk "/$FOCUSED_WINDOW/{p=1;next} p")
-
-  fi
-
-  WORKSPACE_FIRST_MATCHING_WINDOW=$(echo $WORKSPACE_MATCHING_WINDOWS | tr ' ' \\n | head -n1)
-
-  if [ -n "$WORKSPACE_FIRST_MATCHING_WINDOW" ]; then
-    xdotool windowactivate $WORKSPACE_FIRST_MATCHING_WINDOW
+  if [ -n "$MATCHING_WORKSPACE_WINDOW" ]; then
+    xdotool windowactivate $MATCHING_WORKSPACE_WINDOW
     break
   fi
 
