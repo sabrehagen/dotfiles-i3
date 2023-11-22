909d7e9340b716d91ac30928ae4418b4b7733463
Jackson Delahunt <jackson@stemn.com>
2023-11-22 22:26:52 +1100

    less-sleep

--- .config/i3/save-layout.sh
+++ .config/i3/save-layout.sh
@@ -5,7 +5,7 @@ rm -rf $RESURRECT_DIRECTORY/*
 
 # Kill any i3bar build log windows before saving
 tmux kill-session -t i3bar_desktop-environment-build*
-sleep 0.5
+sleep 0.1
 
 # Save all workspaces
 i3-msg -t get_workspaces | \
