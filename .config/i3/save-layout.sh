df02e7a397531adf02537cd3536e7a1116b26b8b
Jackson Delahunt <jackson@stemn.com>
2023-11-22 22:20:46 +1100

    no-save-build-log

--- .config/i3/save-layout.sh
+++ .config/i3/save-layout.sh
@@ -3,6 +3,9 @@ RESURRECT_DIRECTORY=$HOME/.config/i3/i3-resurrect
 # Clear previous workspace history
 rm -rf $RESURRECT_DIRECTORY/*
 
+# Kill any i3bar build log windows before saving
+tmux kill-session -t i3bar_desktop-environment-build*
+
 # Save all workspaces
 i3-msg -t get_workspaces | \
   jq '.[].name' | \
