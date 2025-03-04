108b7cb0050ff21a96f8b9cf57bf3ce7a3b6662e
Jackson Delahunt <jackson@stemn.com>
2025-03-04 18:06:26 +1100

    cachebust-naming

--- .config/i3/status.sh
+++ .config/i3/status.sh
@@ -1,6 +1,6 @@
 i3status | while :
 do
   read line
-  DESKTOP_ENVIRONMENT_BUILD=$(timeago $(cat $HOME/repositories/sabrehagen/desktop-environment/.dotfiles-cachebust))
+  DESKTOP_ENVIRONMENT_BUILD=$(timeago $(cat $HOME/repositories/sabrehagen/desktop-environment/.cachebust-dotfiles))
   echo "Built $DESKTOP_ENVIRONMENT_BUILD | $line" || exit 1
 done
