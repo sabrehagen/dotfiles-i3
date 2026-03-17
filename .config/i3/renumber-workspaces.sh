84f697cbef1453b2e8837c7cd439e3a2b2660ba5
Jackson Delahunt <jackson@stemn.com>
2026-03-17 19:58:15 +1100

    pgrep

--- .config/i3/renumber-workspaces.sh
+++ .config/i3/renumber-workspaces.sh
@@ -17,7 +17,7 @@ renumber_workspaces() {
 }
 
 # Avoid conflicting with workspace restoration on startup
-until ps aux | grep -v grep | grep -vqz i3-resurrect; do sleep 1; done
+until ! pgrep -x i3-resurrect > /dev/null; do sleep 0.1; done
 
 # Trigger renumber whenever a workspace event occurs
 i3-msg -t subscribe -m '[ "workspace" ]' | \
