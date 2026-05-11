4a9322096fee965a78f2ce7959d36e5680292ff1
Jackson Delahunt <jackson@stemn.com>
2026-05-12 08:38:17 +1000

    stateless-corner-cycle

--- .config/i3/cycle-floating-corner.sh
+++ .config/i3/cycle-floating-corner.sh
@@ -1,23 +1,24 @@
+STEP=${1:-1}
 MARGIN=24
-STATE_FILE=$HOME/.cache/i3-floating-corner-cycle
 
-WIN=$(i3-msg --type get_tree | jq --raw-output 'recurse(.nodes[]?, .floating_nodes[]?) | select(.sticky == true) | "\(.id) \(.rect.width) \(.rect.height)"' | head -n 1)
-test ${WIN:+x} || exit 0
-WIN_ID=${WIN%% *}
-REST=${WIN#* }
-WIN_W=${REST% *}
-WIN_H=${REST#* }
+set -- $(i3-msg --type get_tree | jq --raw-output 'recurse(.nodes[]?, .floating_nodes[]?) | select(.sticky == true) | "\(.id) \(.rect.x) \(.rect.y) \(.rect.width) \(.rect.height)"' | head -n 1)
+test $# -eq 5 || exit 0
+WIN_ID=$1 WIN_X=$2 WIN_Y=$3 WIN_W=$4 WIN_H=$5
 
-WS=$(i3-msg --type get_workspaces | jq --raw-output '.[] | select(.focused) | "\(.rect.width) \(.rect.height)"')
-WS_W=${WS% *}
-WS_H=${WS#* }
+set -- $(i3-msg --type get_workspaces | jq --raw-output '.[] | select(.focused) | "\(.rect.width) \(.rect.height)"')
+WS_W=$1 WS_H=$2
 
 LEFT_X=$MARGIN
 TOP_Y=$MARGIN
-RIGHT_X=$(expr $WS_W - $WIN_W - $MARGIN)
-BOTTOM_Y=$(expr $WS_H - $WIN_H - $MARGIN)
+RIGHT_X=$(( WS_W - WIN_W - MARGIN ))
+BOTTOM_Y=$(( WS_H - WIN_H - MARGIN ))
 
-INDEX=$(cat $STATE_FILE 2>/dev/null || echo 0)
+CX=$(( WIN_X + WIN_W / 2 ))
+CY=$(( WIN_Y + WIN_H / 2 ))
+RIGHT=$(( CX * 2 >= WS_W ))
+BOTTOM=$(( CY * 2 >= WS_H ))
+INDEX=$(( 2 * BOTTOM + (RIGHT + BOTTOM) % 2 ))
+INDEX=$(( (INDEX + STEP + 4) % 4 ))
 
 case $INDEX in
   0) X=$LEFT_X;  Y=$TOP_Y    ;;
@@ -27,4 +28,3 @@ case $INDEX in
 esac
 
 i3-msg "[con_id=$WIN_ID] move position $X $Y"
-echo $(expr \( $INDEX + 1 \) % 4) > $STATE_FILE
